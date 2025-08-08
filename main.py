c80514d730ae7843c803ad02836dfc858cde5358
joshfields2024-beep <joshfields2024@gmail.com>
2025-08-08 09:14:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import json
 from scrapers.scraper_oglasi_rs import search_oglasi_rs
-from telegram_bot import send_telegram_message
-
-def load_settings():
-    with open("settings.json", "r", encoding="utf-8") as f:
-        return json.load(f)
 
 def main():
-    print("[DEBUG] Pokrećem Selenium scraper preko Browserless")
-    settings = load_settings()
-    results = search_oglasi_rs(settings)
-    if not results:
-        send_telegram_message("⚠️ Nema pronađenih oglasa koji ispunjavaju tvoje kriterijume.")
-    else:
-        for res in results:
-            send_telegram_message(res)
+    settings = {}
+    result = search_oglasi_rs(settings)
+    print(result)
 
 if __name__ == "__main__":
     main()
